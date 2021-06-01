2ef8c77e764585979531aa3c7ab900fd04a12ada
Forb.Jok <forbjok@gmail.com>
2021-06-01 18:30:01 +0200

    feat: Update bevy_ecs_tilemap to v0.4.0

--- src/bin/bevy_ecs_tilemap.rs
+++ src/bin/bevy_ecs_tilemap.rs
@@ -1,7 +1,10 @@
 use std::time::Instant;
 
 use bevy::prelude::*;
-use bevy_ecs_tilemap::{Map, Tile, TilemapPlugin};
+use bevy_ecs_tilemap::{
+    prelude::{LayerBuilder, MapQuery, TileBundle},
+    LayerSettings, Map, Tile, TilemapPlugin,
+};
 
 const TILEMAP_WIDTH: u32 = 1024;
 const TILEMAP_HEIGHT: u32 = 1024;
@@ -19,28 +22,27 @@ fn main() {
         .run();
 }
 
-fn update_tiles_system(mut commands: Commands, mut query: Query<&mut Map>, mut count: Local<u32>) {
+fn update_tiles_system(mut commands: Commands, mut count: Local<u32>, mut map_query: MapQuery) {
     *count += 1;
 
     let upd_tiles = Instant::now();
 
-    for mut tilemap in query.iter_mut() {
-        let mut i = *count % 4;
+    let mut i = *count % 4;
 
-        for y in 0..TILEMAP_HEIGHT {
-            let sprite_index = i % 4;
+    for y in 0..TILEMAP_HEIGHT {
+        let sprite_index = i % 4;
 
-            for x in 0..TILEMAP_WIDTH {
-                let tile = Tile {
-                    texture_index: sprite_index,
-                    ..Default::default()
-                };
+        for x in 0..TILEMAP_WIDTH {
+            let tile_pos = UVec2::new(x, y);
+            let tile = Tile {
+                texture_index: sprite_index as u16,
+                ..Default::default()
+            };
 
-                tilemap.add_tile(&mut commands, UVec2::new(x, y), tile, true).unwrap();
-            }
-
-            i += 1;
+            map_query.set_tile(&mut commands, tile_pos, tile, 0u16, 0u16).unwrap();
         }
+
+        i += 1;
     }
 
     dbg!(upd_tiles.elapsed());
@@ -49,47 +51,47 @@ fn update_tiles_system(mut commands: Commands, mut query: Query<&mut Map>, mut c
 fn setup(
     asset_server: Res<AssetServer>,
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
     mut color_materials: ResMut<Assets<ColorMaterial>>,
+    mut map_query: MapQuery,
 ) {
     let texture_handle = asset_server.load("textures/tilesheet.png");
-    //let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 4, 1);
-    //let texture_atlas_handle = texture_atlases.add(texture_atlas);
     let color_material_handle = color_materials.add(ColorMaterial::texture(texture_handle));
 
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     // Create tilemap (bevy_ecs_tilemap)
-    {
-        let tile_size = Vec2::new(TILE_WIDTH as f32, TILE_HEIGHT as f32);
-        let texture_size = Vec2::new(64.0, 16.0);
 
-        let mut map_settings = bevy_ecs_tilemap::MapSettings::new(
-            UVec2::new(TILEMAP_WIDTH / CHUNK_WIDTH, TILEMAP_HEIGHT / CHUNK_HEIGHT),
-            UVec2::new(CHUNK_WIDTH, CHUNK_HEIGHT),
-            tile_size,
-            texture_size,
-            0,
-        );
+    let tile_size = Vec2::new(TILE_WIDTH as f32, TILE_HEIGHT as f32);
+    let texture_size = Vec2::new(64.0, 16.0);
 
-        // Disable culling as a workaround for bug caused by not using bevy_ecs_tilemap not using GlobalTransform
-        map_settings.cull = false;
+    let mut layer_settings = LayerSettings::new(
+        UVec2::new(TILEMAP_WIDTH / CHUNK_WIDTH, TILEMAP_HEIGHT / CHUNK_HEIGHT),
+        UVec2::new(CHUNK_WIDTH, CHUNK_HEIGHT),
+        tile_size,
+        texture_size,
+    );
 
-        let mut map = bevy_ecs_tilemap::Map::new(map_settings);
+    // Disable culling
+    layer_settings.cull = false;
 
-        // Map
-        let map_entity = commands.spawn().id();
+    let (layer_builder, layer_entity) = LayerBuilder::<TileBundle>::new(&mut commands, layer_settings, 0u16, 0u16);
 
-        map.build(&mut commands, &mut meshes, color_material_handle, map_entity, false);
+    map_query.build_layer(&mut commands, layer_builder, color_material_handle);
 
-        commands.entity(map_entity).insert_bundle(bevy_ecs_tilemap::MapBundle {
-            map,
-            transform: Transform {
-                scale: Vec3::splat(1.0),
-                translation: Vec3::new(-640.0, -360.0, 0.0),
-                ..Default::default()
-            },
+    // Create map entity and component:
+    let map_entity = commands.spawn().id();
+
+    let mut map = Map::new(0u16, map_entity);
+    map.add_layer(&mut commands, 0u16, layer_entity);
+
+    // Insert Map component in map entity
+    commands
+        .entity(map_entity)
+        .insert(map)
+        .insert(Transform {
+            scale: Vec3::splat(1.0),
+            translation: Vec3::new(-640.0, -360.0, 0.0),
             ..Default::default()
-        });
-    };
+        })
+        .insert(GlobalTransform::default());
 }
